74bee1381b8a4423e2cf12ffeea62464db367cbe
fallupboy <svity21@gmail.com>
2020-10-23 12:46:07 +0300

    init

---
+++ SimpleCalculator/Form1.cs
@@ -0,0 +1,354 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SimpleCalculator
+{
+    public partial class Form1 : Form
+    {
+        #region Constructor
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Clearing Methods
+
+        private void CEButton_Click(object sender, EventArgs e)
+        {
+            // Clear the text
+            this.UserInputText.Text = string.Empty;
+            FocusInputText();
+        }
+
+        private void DelButton_Click(object sender, EventArgs e)
+        {
+            DeleteTextValue();
+            FocusInputText();
+        }
+
+        #endregion
+
+        #region Operator Methods
+        private void DivideButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("/");
+            FocusInputText();
+        }
+        private void MultButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("*");
+            FocusInputText();
+        }
+
+        private void MinusButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("-");
+            FocusInputText();
+        }
+
+        private void PlusButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("+");
+            FocusInputText();
+        }
+
+        private void EqualsButton_Click(object sender, EventArgs e)
+        {
+            CalculateEquation();
+            FocusInputText();
+        }
+
+        #endregion
+
+        #region Number Methods
+        private void DotButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue(",");
+            FocusInputText();
+        }
+
+        private void ZeroButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("0");
+            FocusInputText();
+        }
+
+        private void OneButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("1");
+            FocusInputText();
+        }
+
+        private void TwoButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("2");
+            FocusInputText();
+        }
+
+        private void ThreeButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("3");
+            FocusInputText();
+        }
+
+        private void FourButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("4");
+            FocusInputText();
+        }
+
+        private void FiveButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("5");
+            FocusInputText();
+        }
+
+        private void SixButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("6");
+            FocusInputText();
+        }
+
+        private void SevenButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("7");
+            FocusInputText();
+        }
+
+        private void EightButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("8");
+            FocusInputText();
+        }
+
+        private void NineButton_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("9");
+            FocusInputText();
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Calculates the given equation and outputh the answer to the user label
+        /// </summary>
+        private void CalculateEquation()
+        {
+            this.CalculationResultText.Text = ParseOperation();
+            FocusInputText();
+
+        }
+
+        /// <summary>
+        /// Parses the users equation and calculates the result
+        /// </summary>
+        /// <returns></returns>
+        private string ParseOperation()
+        {
+            try
+            {
+                // Get the users equation input
+                var input = this.UserInputText.Text;
+
+                // Remove all spaces
+                input = input.Replace(" ", "");
+
+                // Create a new top-level operation
+                var operation = new Operation();
+                var leftSide = true;
+
+
+                // Loop through each character of the input
+                // starting from the left working to the right
+                for (int i = 0; i < input.Length; i++)
+                {
+                    // TODO: Handle order priority
+                    //       4 + 5 * 3
+                    //       It should calculate 5 * 3 first, then 4 + result (so 4 + 15) 
+
+                    // Check if the current character is a number
+                    if ("0123456789,".Any(c => input[i] == c))
+                    {
+                        if (leftSide)
+                            operation.LeftSide = AddNumberPart(operation.LeftSide, input[i]);
+                        else
+                            operation.RightSide = AddNumberPart(operation.RightSide, input[i]);
+                    }
+
+                    // If it is an operator ( + - * / ) set the operator type
+                    else if ("+-*/".Any(c => input[i] == c))
+                    {
+                        // If we are on the right side already, we now need to calculate our current operation
+                        // and set the result to the left side of the next operation
+                        if (!leftSide)
+                        {
+                            // Get the operator type 
+                            var operatorType = GetOperationType(input[i]);
+
+                            // Check if we actually have a right side numebr
+                            if (operation.RightSide.Length == 0)
+                            {
+                                // Check the operator is not a minus (as they could be creating a negative number)
+                                if (operatorType != OperationType.Minus)
+                                    throw new InvalidOperationException($"Operator (+ * / or more than one -) specified without and right side number");
+
+                                // If we got here, the operator type is a minus, and there is no left number currently, so add the minus to the number
+                                operation.RightSide += input[i];
+                            }
+                            else
+                            {
+                                // Calculate previous equation and set it to the left side
+                                operation.LeftSide = CalculateOperation(operation);
+
+                                // Set new operator 
+                                operation.OperationType = operatorType;
+
+                                // Clear the previous right number 
+                                operation.RightSide = string.Empty;
+                            }
+                        }
+                        else
+                        {
+                            // Get the operatop type
+                            var operatorType = GetOperationType(input[i]);
+
+                            // Check if we actually have a left side numebr
+                            if (operation.LeftSide.Length == 0)
+                            {
+                                // Check the operator is not a minus (as they could be creating a negative number)
+                                if (operatorType != OperationType.Minus)
+                                    throw new InvalidOperationException($"Operator (+ * / or more than one -) specified without and left side number");
+
+                                // If we got here, the operator type is a minus, and there is no left number currently, so add the minus to the number
+                                operation.LeftSide += input[i];
+                            }
+                            else
+                            {
+                                // If we get here, we have a left number and now an operator, so we want to move to the right side
+
+                                // Set the operation type
+                                operation.OperationType = operatorType;
+
+                                // Move to the right side
+                                leftSide = false;
+                            }
+                        }
+                    }
+                }
+
+                // If we are done parsing, and there were no exceptions
+                // calculate the current operation
+                return CalculateOperation(operation);
+            }
+            catch (Exception ex)
+            {
+                return $"Invalid equation. {ex.Message}";
+            }
+        }
+
+        /// <summary>
+        /// Calculates a <see cref="Operation"/> and returnes the result
+        /// </summary>
+        /// <param name="operation">The operation to calculate</param>
+        private string CalculateOperation(Operation operation)
+        {
+            // Store the number values of the string representations
+            decimal left = 0;
+            decimal right = 0;
+
+            // Check if we have a valid left side number
+            if (string.IsNullOrEmpty(operation.LeftSide) || !decimal.TryParse(operation.LeftSide, out left))
+                throw new InvalidOperationException($"Left side of the operation was not the number. {operation.LeftSide}");
+
+            // Check if we have a valid right side number
+            if (string.IsNullOrEmpty(operation.RightSide) || !decimal.TryParse(operation.RightSide, out right))
+                throw new InvalidOperationException($"Right side of the operation was not the number. {operation.RightSide}");
+
+            try
+            {
+                switch (operation.OperationType)
+                {
+                    case OperationType.Add:
+                        return (left + right).ToString();
+                    case OperationType.Minus:
+                        return (left - right).ToString();
+                    case OperationType.Divide:
+                        return (left / right).ToString();
+                    case OperationType.Multiply:
+                        return (left * right).ToString();
+                    default:
+                        throw new InvalidOperationException($"Unknown operator type when calculating operation. { operation.OperationType }");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"Failed to calculate operation {operation.LeftSide} {operation.OperationType} {operation.RightSide}. {ex.Message}");
+            }
+        }
+
+        /// <summary>
+        /// Accepts the character and returns the known <see cref="OperationType"/>
+        /// </summary>
+        /// <param name="character">The character to parse</param>
+        /// <returns></returns>
+        private OperationType GetOperationType(char character)
+        {
+            switch (character)
+            {
+                case '+':
+                    return OperationType.Add;
+                case '-':
+                    return OperationType.Minus;
+                case '/':
+                    return OperationType.Divide;
+                case '*':
+                    return OperationType.Multiply;
+                default:
+                    throw new InvalidOperationException($"Unknown operator type { character }");
+            }
+        }
+
+        /// <summary>
+        /// Attemps to add a new character to the current number, checking for valid characters as it goes
+        /// </summary>
+        /// <param name="currentNumber">The current number string</param>
+        /// <param name="newCharacter">The new character to append to the string</param>
+        /// <returns></returns>
+        private string AddNumberPart(string currentNumber, char newCharacter)
+        {
+            // Check if there is already a . in the number
+            if (newCharacter == ',' && currentNumber.Contains(','))
+                throw new InvalidOperationException($"Number {currentNumber} already contains a . and another cannot be added");
+
+            return currentNumber + newCharacter;
+        }
+
+        #region Private Helpers
+
+        private void FocusInputText()
+        {
+            this.UserInputText.Focus();
+        }
+
+        private void InsertTextValue(string value)
+        {
+            var selectionStart = this.UserInputText.SelectionStart;
+            this.UserInputText.Text = this.UserInputText.Text.Insert(this.UserInputText.SelectionStart, value);
+            this.UserInputText.SelectionStart = selectionStart + value.Length;
+        }
+
+        private void DeleteTextValue()
+        {
+            if (this.UserInputText.Text.Length < this.UserInputText.SelectionStart + 1)
+                return;
+            var selectionStart = this.UserInputText.SelectionStart;
+            this.UserInputText.Text = this.UserInputText.Text.Remove(this.UserInputText.SelectionStart, 1);
+            this.UserInputText.SelectionStart = selectionStart;
+        }
+        #endregion
+    }
+}
